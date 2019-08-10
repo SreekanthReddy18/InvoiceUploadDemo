3e079895a11d986f1e4b285ef3fe6af8fb888d01
SreekanthReddy18 <sreekanth3143@gmail.com>
2019-08-10 22:53:45 +0530

    Initial commit after working example

---
+++ src/main/java/com/Invoice/upload/InvoiceUploadDemo/Invoice.java
@@ -0,0 +1,26 @@
+package com.Invoice.upload.InvoiceUploadDemo;
+
+import java.io.Serializable;
+
+public class Invoice implements Serializable {
+	private String invoiceNumber;
+	  private String agent;
+  public String getInvoiceNumber() {
+		return invoiceNumber;
+	}
+	public void setInvoiceNumber(String invoiceNumber) {
+		this.invoiceNumber = invoiceNumber;
+	}
+	public String getAgent() {
+		return agent;
+	}
+	public void setAgent(String agent) {
+		this.agent = agent;
+	}
+	@Override
+	public String toString() {
+		return invoiceNumber +","+ agent;
+	}
+
+  
+}
---
+++ src/main/java/com/Invoice/upload/InvoiceUploadDemo/InvoiceUploadDemoApplication.java
@@ -0,0 +1,69 @@
+package com.Invoice.upload.InvoiceUploadDemo;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+import javax.xml.ws.RespectBinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class InvoiceUploadDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceUploadDemoApplication.class, args);
+	}
+
+	@RequestMapping(value = "/create/invoice", method = RequestMethod.POST)
+	public ResponseEntity<String> createInvoice(@RequestBody Invoice invoice) throws IOException {
+		System.out.println("Entered: " + invoice.toString());
+		BufferedWriter out = null;
+		String fileSeparator = System.getProperty("file.separator");
+
+		// absolute file name with path
+		String absoluteFilePath = "F:" + fileSeparator + "MyInvoiceFile"+System.currentTimeMillis()+".xml";
+		File file = null;
+
+		try {
+			file = new File(absoluteFilePath);
+			if (!file.exists()) {
+				if (file.createNewFile()) {
+					System.out.println(absoluteFilePath + " File Created");
+				}
+			}
+			FileWriter fstream = new FileWriter(file, true);
+			out = new BufferedWriter(fstream);
+			out.write(invoice.toString());
+			out.newLine();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (out != null)
+					out.close();
+			} catch (Exception ex) {
+
+			}
+		}
+
+		// return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).build();
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+}
